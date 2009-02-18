1240ba0987d61bc97f3b212ef977e62146c55df0
yoichiro <yoichiro@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-02-18 02:43:11 +0000

    #104 #108

---
+++ jp.eisbahn.eclipse.plugins.osde/src/jp/eisbahn/eclipse/plugins/osde/internal/OsdePreferenceInitializer.java
@@ -0,0 +1,23 @@
+package jp.eisbahn.eclipse.plugins.osde.internal;
+
+import java.util.Locale;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.osgi.service.prefs.Preferences;
+
+public class OsdePreferenceInitializer extends AbstractPreferenceInitializer {
+
+	public OsdePreferenceInitializer() {
+		super();
+	}
+
+	@Override
+	public void initializeDefaultPreferences() {
+		Preferences node = new DefaultScope().getNode(Activator.PLUGIN_ID);
+		Locale locale = Locale.getDefault();
+		node.put(OsdeConfig.DEFAULT_COUNTRY, locale.getCountry());
+		node.put(OsdeConfig.DEFAULT_LANGUAGE, locale.getLanguage());
+	}
+
+}
