203c16277cf3f8d5b334e247bea71638992e4d07
yoichiro <yoichiro@eisbahn.jp>
2009-06-15 23:56:19 +0000

    Fixed issue #14

--- jp.eisbahn.eclipse.plugins.osde/src/jp/eisbahn/eclipse/plugins/osde/internal/runtime/RunAction.java
+++ jp.eisbahn.eclipse.plugins.osde/src/jp/eisbahn/eclipse/plugins/osde/internal/runtime/RunAction.java
@@ -17,41 +17,18 @@
  */
 package jp.eisbahn.eclipse.plugins.osde.internal.runtime;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.util.List;
-
 import jp.eisbahn.eclipse.plugins.osde.internal.Activator;
-import jp.eisbahn.eclipse.plugins.osde.internal.ConnectionException;
-import jp.eisbahn.eclipse.plugins.osde.internal.shindig.ApplicationService;
-import jp.eisbahn.eclipse.plugins.osde.internal.shindig.PersonService;
 import jp.eisbahn.eclipse.plugins.osde.internal.shindig.ShindigLauncher;
-import jp.eisbahn.eclipse.plugins.osde.internal.utils.ApplicationInformation;
-import jp.eisbahn.eclipse.plugins.osde.internal.utils.OpenSocialUtil;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.shindig.social.opensocial.model.Person;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.xml.sax.SAXException;
 
 public class RunAction extends AbstractRunAction implements IObjectActionDelegate, IWorkbenchWindowActionDelegate {
 
@@ -72,7 +49,7 @@ public class RunAction extends AbstractRunAction implements IObjectActionDelegat
 		shell = targetPart.getSite().getShell();
 		this.targetPart = targetPart;
 	}
-
+	
 	/**
 	 * @see IActionDelegate#run(IAction)
 	 */
@@ -82,53 +59,13 @@ public class RunAction extends AbstractRunAction implements IObjectActionDelegat
 			if (result) {
 				shell.getDisplay().timerExec(10000, new Runnable() {
 					public void run() {
-						launch();
+						launch(gadgetXmlFile, project);
 					}
 				});
 				return;
 			}
 		} else {
-			launch();
-		}
-	}
-	
-	private void launch() {
-		try {
-			ApplicationInformation appInfo = OpenSocialUtil.createApplicationInformation(gadgetXmlFile);
-			ApplicationService applicationService = Activator.getDefault().getApplicationService();
-			applicationService.storeAppInfo(appInfo);
-			//
-			PersonService personService = Activator.getDefault().getPersonService();
-			List<Person> people = personService.getPeople();
-			RunApplicationDialog dialog = new RunApplicationDialog(shell, people, gadgetXmlFile);
-			if (dialog.open() == RunApplicationDialog.OK) {
-				Job job = new CreateWebContextJob("Create web context");
-				job.schedule();
-				String view = dialog.getView();
-				String viewer = dialog.getViewer();
-				String owner = dialog.getOwner();
-				String width = dialog.getWidth();
-				String appId = appInfo.getAppId();
-				boolean useExternalBrowser = dialog.isUseExternalBrowser();
-				String country = dialog.getCountry();
-				String language = dialog.getLanguage();
-				LaunchApplicationInformation information = new LaunchApplicationInformation(
-						viewer, owner, view, width, appId, useExternalBrowser,
-						country, language, project, gadgetXmlFile.getName(),
-						project.getName() + ":" + gadgetXmlFile.getName());
-				job = new LaunchApplicationJob("Running application", information, shell);
-				job.schedule(1000);
-				notifyUserPrefsView(information);
-				Activator.getDefault().setLastApplicationInformation(information);
-			}
-		} catch(ConnectionException e) {
-			MessageDialog.openError(shell, "Error", "Shindig database not started yet.");
-		} catch (CoreException e) {
-			MessageDialog.openError(shell, "Error", "Invalid gadget file. " + e.getMessage());
-		} catch (IOException e) {
-			MessageDialog.openError(shell, "Error", "Invalid gadget file. " + e.getMessage());
-		} catch (SAXException e) {
-			MessageDialog.openError(shell, "Error", "Invalid gadget file. " + e.getMessage());
+			launch(gadgetXmlFile, project);
 		}
 	}
 	
@@ -147,39 +84,6 @@ public class RunAction extends AbstractRunAction implements IObjectActionDelegat
 	}
 
 	public void dispose() {
-	}
-
-	private class CreateWebContextJob extends Job {
-
-		private CreateWebContextJob(String name) {
-			super(name);
-		}
-
-		@Override
-		protected IStatus run(IProgressMonitor monitor) {
-			FileOutputStream fos = null;
-			try {
-				InputStreamReader in = new InputStreamReader(getClass().getResourceAsStream("/shindig/context.tmpl"), "UTF-8");
-				StringWriter out = new StringWriter();
-				IOUtils.copy(in, out);
-				String code = out.toString();
-				code = code.replace("$project_name$", project.getName());
-				IPath location = project.getFolder("target").getLocation();
-				code = code.replace("$context_dir$", location.toOSString());
-				File file = new File(Activator.getDefault().getOsdeConfiguration().getJettyDir(), "osde_context_" + project.getName() + ".xml");
-				fos = new FileOutputStream(file);
-				ByteArrayInputStream bytes = new ByteArrayInputStream(code.getBytes("UTF-8"));
-				IOUtils.copy(bytes, fos);
-				return Status.OK_STATUS;
-			} catch(IOException e) {
-				// TODO
-				e.printStackTrace();
-				throw new IllegalStateException(e);
-			} finally {
-				IOUtils.closeQuietly(fos);
-			}
-		}
-		
 	}
 
 }
