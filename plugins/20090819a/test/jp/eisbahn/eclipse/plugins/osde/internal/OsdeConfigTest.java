001517b94ea329c9d6a6a0d9dc3a87212a2c1441
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-08-19 08:19:15 +0000

    Added unit-test stub classes. These classes are auto-generated, so there are some compilation warnings and the code might not follow our code conventions. Please ignore these issues for now.

---
+++ plugins/20090819a/test/jp/eisbahn/eclipse/plugins/osde/internal/OsdeConfigTest.java
@@ -0,0 +1,212 @@
+package jp.eisbahn.eclipse.plugins.osde.internal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * TODO: add javadoc for type
+ */
+public class OsdeConfigTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getExternalDatabaseName()}.
+     */
+    @Test
+    public void testGetExternalDatabaseName() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setExternalDatabaseName(java.lang.String)}.
+     */
+    @Test
+    public void testSetExternalDatabaseName() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#isUseInternalDatabase()}.
+     */
+    @Test
+    public void testIsUseInternalDatabase() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setUseInternalDatabase(boolean)}.
+     */
+    @Test
+    public void testSetUseInternalDatabase() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getExternalDatabaseType()}.
+     */
+    @Test
+    public void testGetExternalDatabaseType() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setExternalDatabaseType(java.lang.String)}.
+     */
+    @Test
+    public void testSetExternalDatabaseType() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getExternalDatabaseHost()}.
+     */
+    @Test
+    public void testGetExternalDatabaseHost() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setExternalDatabaseHost(java.lang.String)}.
+     */
+    @Test
+    public void testSetExternalDatabaseHost() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getExternalDatabasePort()}.
+     */
+    @Test
+    public void testGetExternalDatabasePort() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setExternalDatabasePort(java.lang.String)}.
+     */
+    @Test
+    public void testSetExternalDatabasePort() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getExternalDatabaseUsername()}.
+     */
+    @Test
+    public void testGetExternalDatabaseUsername() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setExternalDatabaseUsername(java.lang.String)}.
+     */
+    @Test
+    public void testSetExternalDatabaseUsername() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getExternalDatabasePassword()}.
+     */
+    @Test
+    public void testGetExternalDatabasePassword() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setExternalDatabasePassword(java.lang.String)}.
+     */
+    @Test
+    public void testSetExternalDatabasePassword() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getJettyDir()}.
+     */
+    @Test
+    public void testGetJettyDir() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setJettyDir(java.lang.String)}.
+     */
+    @Test
+    public void testSetJettyDir() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getDocsSiteMap()}.
+     */
+    @Test
+    public void testGetDocsSiteMap() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setDocsSiteMap(java.util.Map)}.
+     */
+    @Test
+    public void testSetDocsSiteMap() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getDatabaseDir()}.
+     */
+    @Test
+    public void testGetDatabaseDir() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setDatabaseDir(java.lang.String)}.
+     */
+    @Test
+    public void testSetDatabaseDir() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getDefaultLanguage()}.
+     */
+    @Test
+    public void testGetDefaultLanguage() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setDefaultLanguage(java.lang.String)}.
+     */
+    @Test
+    public void testSetDefaultLanguage() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#getDefaultCountry()}.
+     */
+    @Test
+    public void testGetDefaultCountry() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdeConfig#setDefaultCountry(java.lang.String)}.
+     */
+    @Test
+    public void testSetDefaultCountry() {
+        // TODO: implement test method
+    }
+
+}
---
+++ plugins/20090819a/test/jp/eisbahn/eclipse/plugins/osde/internal/builders/GadgetBuilderTest.java
@@ -0,0 +1,36 @@
+package jp.eisbahn.eclipse.plugins.osde.internal.builders;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * TODO: add javadoc for type
+ */
+public class GadgetBuilderTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link GadgetBuilder#GadgetBuilder()}.
+     */
+    @Test
+    public void testGadgetBuilder() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link GadgetBuilder#build(int, java.util.Map, org.eclipse.core.runtime.IProgressMonitor)}.
+     */
+    @Test
+    public void testBuildIntMapIProgressMonitor() {
+        // TODO: implement test method
+    }
+
+}
