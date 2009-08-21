39e0a1c9e07312ccffbb0ae63c9c2a5c43464b7d
sccheng <sccheng@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-08-21 02:25:03 +0000

    Minor fixes and refactoring for revision 485

--- plugins/20090820/src/jp/eisbahn/eclipse/plugins/osde/internal/utils/LogListener.java
+++ plugins/20090820/src/jp/eisbahn/eclipse/plugins/osde/internal/utils/LogListener.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package jp.eisbahn.eclipse.plugins.osde.internal.utils;
 
 import java.io.FileWriter;
@@ -17,15 +35,25 @@ import org.eclipse.core.runtime.IStatus;
 public class LogListener implements ILogListener {
 
     private FileWriter fout;
+    private String path; // path under which the log file is located
    
-    public LogListener(){
-        String path = System.getProperty("user.dir");
-        try {
-        	fout = new FileWriter(path + "\\log.log");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Writing log file to " + path + "\\log.log");
+    public LogListener() {
+        path = System.getProperty("java.io.tmpdir");
+        openOutputStream();
+    }
+    
+    public LogListener(String path) {
+    	this.path = path;
+    	openOutputStream();
+    }
+    
+    public void openOutputStream() {
+    	try {
+    		fout = new FileWriter(path + "\\log.log");
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	System.out.println("Writing log file to " + path + "\\log.log");
     }
    
     public void logging(IStatus status, String plugin) {
@@ -38,5 +66,4 @@ public class LogListener implements ILogListener {
             e.printStackTrace();
         }
     }
-
 }
