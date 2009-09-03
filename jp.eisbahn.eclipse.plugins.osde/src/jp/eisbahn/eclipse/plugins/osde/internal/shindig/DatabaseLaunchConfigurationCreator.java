6db8278f9a70e833ebc4612ff7eaf5b712c2e697
yoichiro <yoichiro@eisbahn.jp>
2009-09-03 11:15:53 +0000

    Commit the patch from ChingYiChan.

--- jp.eisbahn.eclipse.plugins.osde/src/jp/eisbahn/eclipse/plugins/osde/internal/shindig/DatabaseLaunchConfigurationCreator.java
+++ jp.eisbahn.eclipse.plugins.osde/src/jp/eisbahn/eclipse/plugins/osde/internal/shindig/DatabaseLaunchConfigurationCreator.java
@@ -43,7 +43,9 @@ import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jface.preference.IPreferenceStore;
 
 public class DatabaseLaunchConfigurationCreator {
-	
+
+	public final static String H2_LIB_PATH = "/libs/h2-1.1.117.jar";
+
 	public void delete(IProgressMonitor monitor) throws CoreException {
 		try {
 			monitor.beginTask("Deleting the launch configuration for Database.", 1);
@@ -59,7 +61,7 @@ public class DatabaseLaunchConfigurationCreator {
 			monitor.done();
 		}
 	}
-	
+
 	public void create(IProgressMonitor monitor) throws CoreException, MalformedURLException, IOException {
 		try {
 			monitor.beginTask("Creating the launch configuration for Database", 3);
@@ -78,7 +80,7 @@ public class DatabaseLaunchConfigurationCreator {
 			IRuntimeClasspathEntry systemLibsEntry = JavaRuntime.newRuntimeContainerClasspathEntry(
 					systemLibs, IRuntimeClasspathEntry.STANDARD_CLASSES);
 			systemLibsEntry.setClasspathProperty(IRuntimeClasspathEntry.BOOTSTRAP_CLASSES);
-			IRuntimeClasspathEntry h2Entry = createRuntimeClasspathEntry("/h2/h2-1.1.104.jar");
+			IRuntimeClasspathEntry h2Entry = createRuntimeClasspathEntry(H2_LIB_PATH);
 			ILaunchConfigurationWorkingCopy wc = type.newInstance(null, "Shindig Database");
 			List<String> classpath = new ArrayList<String>();
 			classpath.add(systemLibsEntry.getMemento());
@@ -104,14 +106,14 @@ public class DatabaseLaunchConfigurationCreator {
 			monitor.done();
 		}
 	}
-	
+
 	private IRuntimeClasspathEntry createRuntimeClasspathEntry(String path) throws MalformedURLException, IOException {
 		URL url = getBundleEntryUrl(path);
 		IRuntimeClasspathEntry entry = JavaRuntime.newArchiveRuntimeClasspathEntry(new Path(url.getPath()));
 		entry.setClasspathProperty(IRuntimeClasspathEntry.USER_CLASSES);
 		return entry;
 	}
-	
+
 	private URL getBundleEntryUrl(String path) throws MalformedURLException, IOException {
 		return FileLocator.toFileURL(new URL(
 				Activator.getDefault().getBundle().getEntry(path).toExternalForm()));
