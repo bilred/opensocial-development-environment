199adc0620db48718cda928c2189033720f414f0
sccheng <sccheng@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-09-14 06:07:39 +0000

    In /branches/sccheng/plugins/20090821, Preference Page UI Fixes

--- branches/sccheng/plugins/20090821/src/jp/eisbahn/eclipse/plugins/osde/internal/preferences/OsdePreferencePage.java
+++ branches/sccheng/plugins/20090821/src/jp/eisbahn/eclipse/plugins/osde/internal/preferences/OsdePreferencePage.java
@@ -18,10 +18,23 @@
 
 package jp.eisbahn.eclipse.plugins.osde.internal.preferences;
 
-import org.eclipse.jface.preference.*;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.ComboFieldEditor;
+import org.eclipse.jface.preference.DirectoryFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.IWorkbench;
 import jp.eisbahn.eclipse.plugins.osde.internal.Activator;
+import jp.eisbahn.eclipse.plugins.osde.internal.utils.OpenSocialUtil;
 
 /**
  * This class represents a preference page that
@@ -42,11 +55,11 @@ import jp.eisbahn.eclipse.plugins.osde.internal.Activator;
 public class OsdePreferencePage
 	extends FieldEditorPreferencePage
 	implements IWorkbenchPreferencePage {
-
+	
 	public OsdePreferencePage() {
 		super(GRID);
 		setPreferenceStore(Activator.getDefault().getPreferenceStore());
-		setDescription("Configurations for OSDE, Shindig, Database");
+		setDescription("Configurations for OSDE, Shindig, and Database");
 	}
 	
 	/**
@@ -55,46 +68,137 @@ public class OsdePreferencePage
 	 * of preferences. Each field editor knows how to save and
 	 * restore itself.
 	 */
-	public void createFieldEditors() {		
-		addField(new StringFieldEditor(PreferenceConstants.DEFAULT_COUNTRY,
-									   "Country",
-									   getFieldEditorParent()));
-		addField(new StringFieldEditor(PreferenceConstants.DEFAULT_LANGUAGE,
-				   					   "Language",
-				   					   getFieldEditorParent()));
-		addField(new BooleanFieldEditor(PreferenceConstants.USE_INTERNAL_DATABASE,
-				   						"Use internal database",
-				   						getFieldEditorParent()));
-		addField(new StringFieldEditor(PreferenceConstants.EXTERNAL_DATABASE_TYPE,
-				   					   "External database type",
-				   					   getFieldEditorParent()));
+	public void createFieldEditors() {
+		Display display = getShell().getDisplay();
+		
+		// Group for Locale
+		Group localeGroup = new Group(getFieldEditorParent(), SWT.NONE);
+		localeGroup.setText("Locale Settings");
+		GridData layoutData = new GridData(GridData.FILL_HORIZONTAL);
+		layoutData.horizontalSpan = 3;
+		localeGroup.setLayoutData(layoutData);
+		localeGroup.setLayout(new GridLayout(2, false));
+		
+		// Construct Country Field
+		final int NUM_COUNTRIES = OpenSocialUtil.COUNTRIES.length;
+		final int NUM_COLUMNS = 2; // one for key and one for value
+		String[][] countryKeysAndValues = new String[NUM_COUNTRIES][NUM_COLUMNS];
+		for (int i = 0; i < NUM_COUNTRIES; ++i) {
+			String key = OpenSocialUtil.COUNTRIES[i];
+			String value = key.substring(key.indexOf('(')+1, key.length()-1);
+			countryKeysAndValues[i][0] = key;
+			countryKeysAndValues[i][1] = value;
+		}
+		ComboFieldEditor countryFieldEditor = new ComboFieldEditor(PreferenceConstants.DEFAULT_COUNTRY,
+				                                  				   "Country",
+				                                  				   countryKeysAndValues,
+				                                  				   localeGroup);
+		addField(countryFieldEditor);
+		
+		// Construct Language Field
+		final int NUM_LANGUAGES = OpenSocialUtil.LANGUAGES.length;
+		String[][] languageKeysAndValues = new String[NUM_LANGUAGES][NUM_COLUMNS];
+		for (int i = 0; i < NUM_LANGUAGES; ++i) {
+			String key = OpenSocialUtil.LANGUAGES[i];
+			String value = key.substring(key.indexOf('(')+1, key.length()-1);
+			languageKeysAndValues[i][0] = key;
+			languageKeysAndValues[i][1] = value;
+		}
+		ComboFieldEditor languageFieldEditor = new ComboFieldEditor(PreferenceConstants.DEFAULT_LANGUAGE,
+				                                   					"Language",
+				                                   					languageKeysAndValues,
+				                                   					localeGroup);
+		addField(languageFieldEditor);
+		
+		// Web Server Settings
+		Group webServerGroup = new Group(getFieldEditorParent(), SWT.NONE);
+		webServerGroup.setText("Web Server Settings");
+		layoutData = new GridData(GridData.FILL_HORIZONTAL);
+		layoutData.horizontalSpan = 3;
+		webServerGroup.setLayoutData(layoutData);
+		webServerGroup.setLayout(new GridLayout(2, false));
+		
+		addField(new DirectoryFieldEditor(PreferenceConstants.JETTY_DIR,
+				   						  "Jetty directory",
+				   						  webServerGroup));
+		
+		// Database group for database related settings
+		Group databaseGroup = new Group(getFieldEditorParent(), SWT.NONE);
+		databaseGroup.setText("External Database Settings");
+		layoutData = new GridData(GridData.FILL_HORIZONTAL);
+		layoutData.horizontalSpan = 3;
+		databaseGroup.setLayoutData(layoutData);
+		databaseGroup.setLayout(new GridLayout(2, false));
+		
+		BooleanFieldEditor useInternalDatabase = new BooleanFieldEditor(PreferenceConstants.USE_INTERNAL_DATABASE,
+				 									 					"Use internal H2 database",
+				 									 					databaseGroup);
+		addField(useInternalDatabase);
+		
+		Label internalDatabaseLabel = new Label(databaseGroup, SWT.NONE);
+		internalDatabaseLabel.setText("Please Note: If you choose to use internal database,\n" +
+									  "You don't have to configure the following settings for external database");
+		internalDatabaseLabel.setBackground(display.getSystemColor(SWT.COLOR_YELLOW));
+		layoutData = new GridData(GridData.FILL_HORIZONTAL);
+		layoutData.horizontalSpan = 2;
+		internalDatabaseLabel.setLayoutData(layoutData);
+		
+		RadioGroupFieldEditor externalDatabaseType = new RadioGroupFieldEditor(PreferenceConstants.EXTERNAL_DATABASE_TYPE,
+														 "External database type",
+														 1,
+														 new String[][] {{"MySQL", "MySQL"},{"Oracle", "Oracle"}},
+														 databaseGroup);
+		addField(externalDatabaseType);	
 		addField(new StringFieldEditor(PreferenceConstants.EXTERNAL_DATABASE_HOST,
 				   					   "Database host",
-				   					   getFieldEditorParent()));
+				   					   databaseGroup));
 		addField(new IntegerFieldEditor(PreferenceConstants.EXTERNAL_DATABASE_PORT,
 				   						"Database port",
-				   						getFieldEditorParent()));
+				   						databaseGroup));
 		addField(new StringFieldEditor(PreferenceConstants.EXTERNAL_DATABASE_NAME,
 				   					   "Database user name",
-				   					   getFieldEditorParent()));
+				   					   databaseGroup));
 		addField(new StringFieldEditor(PreferenceConstants.EXTERNAL_DATABASE_PASSWORD,
 				   					   "Database password",
-				   					   getFieldEditorParent()));
-		addField(new StringFieldEditor(PreferenceConstants.DATABASE_DIR,
+				   					   databaseGroup));
+		addField(new DirectoryFieldEditor(PreferenceConstants.DATABASE_DIR,
 				   					   "Database directory",
-				   					   getFieldEditorParent()));
-		addField(new StringFieldEditor(PreferenceConstants.JETTY_DIR,
-				   					   "Jetty directory",
-				   					   getFieldEditorParent()));
+				   					   databaseGroup));
+		
+		// Group for other settings
+		Group otherGroup = new Group(getFieldEditorParent(), SWT.NONE);
+		otherGroup.setText("Other Settings");
+		layoutData = new GridData(GridData.FILL_HORIZONTAL);
+		layoutData.horizontalSpan = 3;
+		otherGroup.setLayoutData(layoutData);
+		otherGroup.setLayout(new GridLayout(2, false));
 		addField(new StringFieldEditor(PreferenceConstants.DOCS_SITE_MAP,
 				   					   "Docs site map",
-				   					   getFieldEditorParent()));
+				   					   otherGroup));
+		
+		// Put version information at the bottom of the preference page
+		Group versionInfoGroup = new Group(getFieldEditorParent(), SWT.NONE);
+		versionInfoGroup.setText("Version Information");
+		layoutData = new GridData(GridData.FILL_HORIZONTAL);
+		layoutData.horizontalSpan = 3;
+		versionInfoGroup.setLayoutData(layoutData);
+		versionInfoGroup.setLayout(new GridLayout(2, false));
+		
+		Activator activator = Activator.getDefault();
+		String osdeVersion = (String)activator.getBundle().getHeaders().get(org.osgi.framework.Constants.BUNDLE_VERSION);
+		String name = (String)activator.getBundle().getHeaders().get(org.osgi.framework.Constants.BUNDLE_NAME);
+		String shindigVersion = (String)activator.getBundle().getHeaders().get("Bundle-Shindig-Revision");
+		
+		Label osdeVersionLabel = new Label(versionInfoGroup, SWT.NONE);
+		osdeVersionLabel.setText(name + " Version: " + osdeVersion);
+		Label shindigVersionLabel = new Label(versionInfoGroup, SWT.NONE);
+		shindigVersionLabel.setText("Apache Shindig Revision: " + shindigVersion);
 	}
 
 	/**
 	 * The initializer does nothing here
 	 */
-	public void init(IWorkbench workbench) {
+	public void init(IWorkbench workbench) {		
 	}
-	
+
 }
