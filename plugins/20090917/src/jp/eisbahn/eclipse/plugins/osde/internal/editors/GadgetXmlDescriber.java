f6b6d0e6eba004804d33a3fc3305722641e373d5
sccheng <sccheng@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-09-17 07:28:03 +0000

    Create branch /branches/sccheng/plugins/20090917

---
+++ plugins/20090917/src/jp/eisbahn/eclipse/plugins/osde/internal/editors/GadgetXmlDescriber.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package jp.eisbahn.eclipse.plugins.osde.internal.editors;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import jp.eisbahn.eclipse.plugins.osde.internal.Activator;
+
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.core.runtime.content.IContentDescriber;
+import org.eclipse.core.runtime.content.IContentDescription;
+import org.xml.sax.SAXException;
+
+import com.google.gadgets.GadgetXmlParser;
+
+public class GadgetXmlDescriber implements IContentDescriber {
+
+	public int describe(InputStream contents, IContentDescription description) throws IOException {
+		try {
+			GadgetXmlParser parser = Activator.getDefault().getGadgetXmlParser();
+			parser.parse(contents);
+			return IContentDescriber.VALID;
+		} catch (IOException e) {
+			return IContentDescriber.INDETERMINATE;
+		} catch (SAXException e) {
+			return IContentDescriber.INDETERMINATE;
+		}
+		
+	}
+
+	public QualifiedName[] getSupportedOptions() {
+		return new QualifiedName[0];
+	}
+
+}
