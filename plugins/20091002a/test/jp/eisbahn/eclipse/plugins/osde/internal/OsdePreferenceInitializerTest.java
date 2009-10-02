3f0e0e02fb7d8d2896be872c8c066d88a9a50137
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-10-02 04:00:15 +0000

    branch albert plugins 20091002a

---
+++ plugins/20091002a/test/jp/eisbahn/eclipse/plugins/osde/internal/OsdePreferenceInitializerTest.java
@@ -0,0 +1,34 @@
+package jp.eisbahn.eclipse.plugins.osde.internal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * TODO: add javadoc for type
+ */
+public class OsdePreferenceInitializerTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link OsdePreferenceInitializer#initializeDefaultPreferences()}.
+     */
+    @Test
+    public void testInitializeDefaultPreferences() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link OsdePreferenceInitializer#OsdePreferenceInitializer()}.
+     */
+    @Test
+    public void testOsdePreferenceInitializer() {
+        // TODO: implement test method
+    }
+
+}
