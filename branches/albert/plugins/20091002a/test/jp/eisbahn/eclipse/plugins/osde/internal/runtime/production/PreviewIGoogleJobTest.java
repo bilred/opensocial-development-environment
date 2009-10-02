f59ef14309252b6f601c4abd513ec6038ed04288
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-10-02 12:53:56 +0000

    tmp checkin: * rename project w/o spaces * trying to construct an IFile object

--- branches/albert/plugins/20091002a/test/jp/eisbahn/eclipse/plugins/osde/internal/runtime/production/PreviewIGoogleJobTest.java
+++ branches/albert/plugins/20091002a/test/jp/eisbahn/eclipse/plugins/osde/internal/runtime/production/PreviewIGoogleJobTest.java
@@ -20,9 +20,16 @@ package jp.eisbahn.eclipse.plugins.osde.internal.runtime.production;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.apache.http.client.ClientProtocolException;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -33,6 +40,8 @@ import org.junit.Test;
  * @author albert.cheng.ig@gmail.com
  */
 public class PreviewIGoogleJobTest {
+
+    private static Logger logger = Logger.getLogger(PreviewIGoogleJobTest.class.getName());
 
     /**
      * @throws java.lang.Exception
@@ -72,14 +81,34 @@ public class PreviewIGoogleJobTest {
 
     /**
      * Test method for {@link PreviewIGoogleJob#uploadFilesToIg()}.
+     * @throws CoreException
+     * @throws IOException
+     * @throws ClientProtocolException
      */
     @Test
-    public void testUploadFilesToIg() {
+    public void testUploadFilesToIg() throws ClientProtocolException, IOException, CoreException {
         String username = "osde.test.001";
         String password = "osdetest888";
-        IFile gadgetXmlFile = null; // FIXME
+        String path = "test/jp/eisbahn/eclipse/plugins/osde/internal/runtime/"
+                + "production/testdata/gadget.xml";
+        File gadgetXmlFile = new File(path);
+        logger.info("gadgetXmlFile: " + gadgetXmlFile);
+        logger.info("gadgetXmlFile abs path: " + gadgetXmlFile.getAbsolutePath());
+        logger.info("gadgetXmlFile total space: " + gadgetXmlFile.getTotalSpace());
+        logger.info("gadgetXmlFile exists: " + gadgetXmlFile.exists());
+        IPath ipath = Path.fromPortableString(path);
+        logger.info("ipath: " + ipath);
+        logger.info("ipath ext: " + ipath.getFileExtension());
+
+        IFile gadgetXmlIFile = ResourcesPlugin.getWorkspace().getRoot().getFile(ipath);
+        //IFile gadgetXmlIFile = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(ipath);
+        logger.info("gadgetXmlIFile: " + gadgetXmlIFile);
+        logger.info("gadgetXmlIFile name: " + gadgetXmlIFile.getName());
+        logger.info("gadgetXmlIFile raw loc: " + gadgetXmlIFile.getRawLocation()); // FIXME
         PreviewIGoogleJob job = new PreviewIGoogleJob("Preview iGoogle gadget", username,
-                password, false, gadgetXmlFile);
+                password, false, gadgetXmlIFile);
+        String result = job.uploadFilesToIg();
+        logger.info("result: " + result);
     }
 
 }
