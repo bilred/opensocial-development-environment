b37e037d21cdcee78c387f1594d425f184fc57a0
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-10-14 04:01:53 +0000

    branch albert plugins 20091014a

---
+++ plugins/20091014a/test/jp/eisbahn/eclipse/plugins/osde/internal/runtime/production/PreviewIGoogleJobTest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under
+ * the License.
+ */
+package jp.eisbahn.eclipse.plugins.osde.internal.runtime.production;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import jp.eisbahn.eclipse.plugins.osde.internal.utils.HostingException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.Test;
+
+/**
+ * @author albert.cheng.ig@gmail.com
+ */
+public class PreviewIGoogleJobTest {
+
+    private static Logger logger = Logger.getLogger(PreviewIGoogleJobTest.class.getName());
+
+    /**
+     * Test method for {@link PreviewIGoogleJob#run(org.eclipse.core.runtime.IProgressMonitor)}.
+     */
+    @Test
+    public void testRunIProgressMonitor() {
+        // TODO: Implement testRunIProgressMonitor()
+    }
+
+    /**
+     * Test method for {@link PreviewIGoogleJob#uploadFilesToIg()}.
+     * @throws CoreException
+     * @throws IOException
+     * @throws ClientProtocolException
+     * @throws HostingException
+     */
+    @Test
+    public void testUploadFilesToIg()
+            throws ClientProtocolException, IOException, CoreException, HostingException {
+        Shell shell = new Shell();
+        String username = "osde.test.001";
+        String password = "osdetest888";
+        String rootPath = "test/jp/eisbahn/eclipse/plugins/osde/internal/runtime/"
+                + "production/testdata/";
+        String gadgetXmlFileRelativePath = "gadget.xml";
+
+        File gadgetXmlFile = new File(rootPath, gadgetXmlFileRelativePath);
+        logger.info("gadgetXmlFile abs path: " + gadgetXmlFile.getAbsolutePath());
+        PreviewIGoogleJob job = new PreviewIGoogleJob(
+                "Preview iGoogle gadget", shell, username, password, false, gadgetXmlFile);
+        String gadgetPreviewUrl = job.uploadFilesToIg();
+        logger.info("gadgetPreviewUrl: " + gadgetPreviewUrl);
+        assertTrue(gadgetPreviewUrl.endsWith(gadgetXmlFileRelativePath));
+    }
+
+}
