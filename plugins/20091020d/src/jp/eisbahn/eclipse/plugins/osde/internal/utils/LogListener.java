2a74e41ee71c8fbf8d599b9bcf4d27e5e0667bfa
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-10-20 07:44:12 +0000

    branch albert plugins 20091020d

---
+++ plugins/20091020d/src/jp/eisbahn/eclipse/plugins/osde/internal/utils/LogListener.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package jp.eisbahn.eclipse.plugins.osde.internal.utils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import jp.eisbahn.eclipse.plugins.osde.internal.Activator;
+
+import org.eclipse.core.runtime.ILogListener;
+import org.eclipse.core.runtime.IStatus;
+
+/**
+ * Listener class that listens to logging event.
+ * This listener writes logs into a file.
+ * 
+ * @author Sega Shih-Chia Cheng (sccheng@gmail.com, shihchia@google.com)
+ *
+ */
+public class LogListener implements ILogListener {
+
+    private FileWriter fout;
+    private String path; // path under which the log file is located
+   
+    public LogListener() {
+        this(Activator.getDefault().getWorkDirectory().getAbsolutePath());
+    }
+    
+    public LogListener(String path) {
+    	this.path = path;
+    	openOutputStream();
+    }
+    
+    public void openOutputStream() {
+    	File logFile = new File(path, "log.log");
+    	try {
+    		fout = new FileWriter(logFile);
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	System.out.println("Writing log file to " + logFile.getAbsolutePath());
+    }
+   
+    public void logging(IStatus status, String plugin) {
+        try {
+            fout.write(status.getSeverity() + ": " + status.getMessage());
+            fout.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Closes the file writer to ensure there will be no memory leak
+     * This function is called before the activator detaches the log listener
+     */
+    public void dispose() {
+    	try {
+    		fout.close();
+    	} catch (IOException ioe) {
+    		System.err.println("Can't close the file writer in the log listener.");
+    		ioe.printStackTrace();
+    	}
+    }
+}
