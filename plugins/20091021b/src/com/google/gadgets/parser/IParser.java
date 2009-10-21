0cfdf5e867d2f39d763e0438ffdc36040b67aa14
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-10-21 07:29:14 +0000

    branch albert plugins 20091021b

---
+++ plugins/20091021b/src/com/google/gadgets/parser/IParser.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.google.gadgets.parser;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * A dummy interface for parsers
+ * 
+ * All parsers should implement AbstractParser instead of this interface
+ * This interface is intended for implementing a uniform interface
+ * to generate parsers for clients
+ * 
+ * @author Sega Shih-Chia Cheng (sccheng@gmail.com, shihchia@google.com)
+ *
+ */
+public interface IParser {
+	public Object parse(InputStream in) throws ParserException;
+	public Object parse(File fin) throws ParserException;
+}
