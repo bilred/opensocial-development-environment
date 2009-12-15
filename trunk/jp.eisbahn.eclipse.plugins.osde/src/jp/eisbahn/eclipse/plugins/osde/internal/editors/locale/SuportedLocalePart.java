fe004e70fe3bc5608f2a0fc8d4ee67eaf1fdd00f
ChingYiChan.tw <ChingYiChan.tw@fa0fd100-0148-11de-ada4-5153b8187bf2>
2009-12-15 02:58:23 +0000

    merged with branch/chingyichan/plugins/20091213b: enable jetty port number configuration

--- trunk/jp.eisbahn.eclipse.plugins.osde/src/jp/eisbahn/eclipse/plugins/osde/internal/editors/locale/SuportedLocalePart.java
+++ trunk/jp.eisbahn.eclipse.plugins.osde/src/jp/eisbahn/eclipse/plugins/osde/internal/editors/locale/SuportedLocalePart.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import jp.eisbahn.eclipse.plugins.osde.internal.Activator;
 import jp.eisbahn.eclipse.plugins.osde.internal.utils.Logger;
 
 import org.apache.commons.lang.StringUtils;
@@ -64,7 +65,7 @@ import com.google.gadgets.model.Module.ModulePrefs.Locale;
 import com.google.gadgets.model.MessageBundle.Msg;
 
 public class SuportedLocalePart extends SectionPart implements IPartSelectionListener {
-    
+
     private static final Logger logger = new Logger(SuportedLocalePart.class);
 
     private LocalePage page;
@@ -206,7 +207,9 @@ public class SuportedLocalePart extends SectionPart implements IPartSelectionLis
                     ByteArrayInputStream in = new ByteArrayInputStream(msgBundle.toString().getBytes("UTF-8"));
                     bundleFile.create(in, true, new NullProgressMonitor());
 
-                    locale.setMessages("http://localhost:8080/" + project.getName() + "/" + fileName);
+                    locale.setMessages("http://localhost:"
+                    	+ Activator.getDefault().getOsdeConfiguration().getJettyPort()
+                    	+ "/" + project.getName() + "/" + fileName);
                     locale.setMessageBundle(msgBundle);
                 } catch (CoreException e) {
                     logger.warn("Creating the message bundle file failed.", e);
