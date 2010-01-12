3d0ea7069ad64025ca47c2ba9009ee1ebcb8a53e
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2010-01-12 05:22:00 +0000

    branch albert plugins 20100112b

---
+++ branches/albert/plugins/20100112b/test/com/google/gadgets/model/ModuleTest.java
@@ -0,0 +1,48 @@
+package com.google.gadgets.model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ModuleTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public final void testGetModulePrefs() {
+        // TODO: implement test method
+    }
+
+    @Test
+    public final void testSetModulePrefs() {
+        // TODO: implement test method
+    }
+
+    @Test
+    public final void testGetUserPref() {
+        // TODO: implement test method
+    }
+
+    @Test
+    public final void testAddUserPref() {
+        // TODO: implement test method
+    }
+
+    @Test
+    public final void testGetContent() {
+        // TODO: implement test method
+    }
+
+    @Test
+    public final void testAddContent() {
+        // TODO: implement test method
+    }
+
+}
