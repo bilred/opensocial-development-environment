84733e5b2218b948502fbd3be66b2ae9d8ac7122
ChingYiChan.tw <ChingYiChan.tw@fa0fd100-0148-11de-ada4-5153b8187bf2>
2010-01-15 10:26:41 +0000

    implement the osde explorer view

--- plugins/20100115b/osde_plugin/src/com/googlecode/osde/internal/ui/views/NonOsdeProjectFilter.java
+++ plugins/20100115b/osde_plugin/src/com/googlecode/osde/internal/ui/views/NonOsdeProjectFilter.java
@@ -1,11 +1,17 @@
 package com.googlecode.osde.internal.ui.views;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 
-public class ClosedProjectFilter extends ViewerFilter {
+import com.googlecode.osde.internal.OsdeProjectNature;
+import com.googlecode.osde.internal.utils.Logger;
+
+public class NonOsdeProjectFilter extends ViewerFilter {
+	
+	private static Logger logger = new Logger(NonOsdeProjectFilter.class);
 	
 	@Override
 	public boolean select(Viewer viewer, Object parentElement, Object element) {
@@ -14,11 +20,11 @@ public class ClosedProjectFilter extends ViewerFilter {
             Object adapted = adaptable.getAdapter(IProject.class);
             if(adapted instanceof IProject){
                 IProject project = (IProject) adapted;
-                if(project.isOpen()){
-                    return true;
-                }else{
-                    return false;
-                }
+                try {
+					return project.isOpen() && project.hasNature(OsdeProjectNature.ID);
+				} catch (CoreException e) {
+					logger.error(e.getMessage(), e);
+				}
             }
         }
         return true;
