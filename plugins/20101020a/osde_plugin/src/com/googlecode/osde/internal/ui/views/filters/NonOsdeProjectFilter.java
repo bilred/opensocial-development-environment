77096e0f91a9ae9078f9ecf8666b4b59b7946194
ChingYiChan.tw <ChingYiChan.tw@fa0fd100-0148-11de-ada4-5153b8187bf2>
2010-01-20 09:21:35 +0000

    fix code convention and add the license

--- plugins/20101020a/osde_plugin/src/com/googlecode/osde/internal/ui/views/filters/NonOsdeProjectFilter.java
+++ plugins/20101020a/osde_plugin/src/com/googlecode/osde/internal/ui/views/filters/NonOsdeProjectFilter.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.googlecode.osde.internal.ui.views.filters;
 
 import org.eclipse.core.resources.IProject;
@@ -9,22 +26,23 @@ import org.eclipse.jface.viewers.ViewerFilter;
 import com.googlecode.osde.internal.OsdeProjectNature;
 import com.googlecode.osde.internal.utils.Logger;
 
+/**
+ * @author chingyichan.tw@gmail.com (qrtt1)
+ *
+ */
 public class NonOsdeProjectFilter extends ViewerFilter {
-	
-	private static Logger logger = new Logger(NonOsdeProjectFilter.class);
-	
-	@Override
-	public boolean select(Viewer viewer, Object parentElement, Object element) {
-        if(element instanceof IAdaptable){
+
+    private static Logger logger = new Logger(NonOsdeProjectFilter.class);
+
+    @Override
+    public boolean select(Viewer viewer, Object parentElement, Object element) {
+        if (element instanceof IAdaptable) {
             IAdaptable adaptable = (IAdaptable) element;
             Object adapted = adaptable.getAdapter(IProject.class);
-            if(adapted instanceof IProject){
+            if (adapted instanceof IProject) {
                 IProject project = (IProject) adapted;
-                if (!project.isOpen()) {
-                    return true;
-                }
                 try {
-                    return project.hasNature(OsdeProjectNature.ID);
+                    return project.isOpen() && project.hasNature(OsdeProjectNature.ID);
                 } catch (CoreException e) {
                     logger.error(e.getMessage(), e);
                 }
@@ -32,6 +50,6 @@ public class NonOsdeProjectFilter extends ViewerFilter {
         }
         return true;
 
-	}
+    }
 
 }
