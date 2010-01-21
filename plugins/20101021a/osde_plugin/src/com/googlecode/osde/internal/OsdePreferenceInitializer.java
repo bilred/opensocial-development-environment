74fe38ac069dd7386481028953871b3bb6246845
ChingYiChan.tw <ChingYiChan.tw@fa0fd100-0148-11de-ada4-5153b8187bf2>
2010-01-21 06:16:49 +0000

    clean the unused code

--- plugins/20101021a/osde_plugin/src/com/googlecode/osde/internal/OsdePreferenceInitializer.java
+++ plugins/20101021a/osde_plugin/src/com/googlecode/osde/internal/OsdePreferenceInitializer.java
@@ -12,35 +12,5 @@ public class OsdePreferenceInitializer extends AbstractPreferenceInitializer {
         // delegate the initialization to the Config Manager.
         Activator.getDefault().getPreferencesModel().initializeDefaultPreferences();
     }
-    
-//    @Override
-//    public void initializeDefaultPreferences() {
-//        String tmpdir = Activator.getDefault().getWorkDirectory().getAbsolutePath();
-//        Preferences node = new DefaultScope().getNode(Activator.PLUGIN_ID);
-//        Locale locale = Locale.getDefault();
-//        node.put(OsdeConfig.DEFAULT_COUNTRY, locale.getCountry());
-//        node.put(OsdeConfig.DEFAULT_LANGUAGE, locale.getLanguage());
-//        File file = new File(tmpdir, "osde_db");
-//        file.mkdirs();
-//        node.put(OsdeConfig.DATABASE_DIR, file.getAbsolutePath());
-//        node.put(OsdeConfig.DOCS_SITE_MAP, "");
-//        file = new File(tmpdir, "osde_jetty");
-//        file.mkdirs();
-//        node.put(OsdeConfig.JETTY_DIR, file.getAbsolutePath());
-//        node.put(OsdeConfig.USE_INTERNAL_DATABASE, "true");
-//        node.put(OsdeConfig.EXTERNAL_DATABASE_TYPE, "MySQL");
-//        node.put(OsdeConfig.EXTERNAL_DATABASE_HOST, "localhost");
-//        node.put(OsdeConfig.EXTERNAL_DATABASE_PORT, "3306");
-//        node.put(OsdeConfig.EXTERNAL_DATABASE_USERNAME, "");
-//        node.put(OsdeConfig.EXTERNAL_DATABASE_PASSWORD, "");
-//        node.put(OsdeConfig.EXTERNAL_DATABASE_NAME, "osde");
-//        String userHome = System.getProperty("user.home");
-//        file = new File(userHome, Activator.WORK_DIR_NAME);
-//        file.mkdirs();
-//        node.put(OsdeConfig.WORK_DIRECTORY, file.getAbsolutePath());
-//        node.put(OsdeConfig.LOGGER_CONFIG_FILE, "");
-//        node.putBoolean(OsdeConfig.COMPILE_JAVASCRIPT, JdkVersion.isAtLeastJdk6());
-//        node.put(OsdeConfig.FIREFOX_LOCATION, OsdeConfig.DEFAULT_FIREFOX_LOCATION);
-//    }
 
 }
