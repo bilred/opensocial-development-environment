1a9e6ca1be25d97482b6b9a9e54dbacc859666e0
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2010-04-19 07:33:06 +0000

    merged with 0416d: * prepare to support iGoogle user session * add IgCredentials.currentCredentials * fixing issue 209 (this is still work in progress)

--- trunk/osde_plugin/test/com/googlecode/osde/internal/igoogle/AddItJobTest.java
+++ trunk/osde_plugin/test/com/googlecode/osde/internal/igoogle/AddItJobTest.java
@@ -79,7 +79,8 @@ public class AddItJobTest {
 
         // Execute modifyHostingUrlForGadgetFileAndUploadIt().
         IgAddItJob job = new IgAddItJob("preview", gadgetXmlIFile, null, false);
-        IgCredentials igCredentials = new IgCredentials(TEST_USERNAME, TEST_PASSWORD);
+        IgCredentials igCredentials =
+        	    IgCredentials.createCurrentInstance(TEST_USERNAME, TEST_PASSWORD);
         job.modifyHostingUrlForGadgetFileAndUploadIt(IgAddItJob.LOCAL_HOST_URL,
                 NEW_HOSTING_URL, igCredentials, "/osde/preview/"); // TODO: (p1) make string const
 
