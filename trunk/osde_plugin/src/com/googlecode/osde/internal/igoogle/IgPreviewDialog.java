3e4aca0373a04518db74dc8edfcb52228107da03
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2010-05-05 08:38:10 +0000

    merged with branches/albert/plugin_2010/0505a (2nd round): fix wordings

--- trunk/osde_plugin/src/com/googlecode/osde/internal/igoogle/IgPreviewDialog.java
+++ trunk/osde_plugin/src/com/googlecode/osde/internal/igoogle/IgPreviewDialog.java
@@ -97,7 +97,10 @@ public class IgPreviewDialog extends IgCredentialsDialog {
         // Prepare extra description.
         Label extraDescription = new Label(panelComposite, SWT.LEFT);
         extraDescription.setText("\nNote: Some social features will not function properly unless\n"
-        		+ "you select the necessary options in the 'Add gadget to my\niGoogle page' step.");
+        		+ "you select the necessary options in the 'Add gadget to my\n"
+        		+ "iGoogle page' step. These options are:\n"
+                + "\t(1) Know who I am and see my Friends group\n"
+                + "\t(2) Post my activities to Updates\n");
         extraDescription.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
         return dialogAreaComposite;
     }
