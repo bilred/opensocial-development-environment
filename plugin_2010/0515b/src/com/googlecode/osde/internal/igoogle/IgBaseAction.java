d70a88390eebd077b12ad0fe38507da610eae3cf
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2010-05-14 20:52:21 +0000

    branch albert plugin 2010 0515b

---
+++ plugin_2010/0515b/src/com/googlecode/osde/internal/igoogle/IgBaseAction.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under
+ * the License.
+ */
+package com.googlecode.osde.internal.igoogle;
+
+import com.googlecode.osde.internal.utils.Logger;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+/**
+ * The base Action for iGoogle-related features.
+ *
+ * @author albert.cheng.ig@gmail.com
+ */
+public abstract class IgBaseAction
+        implements IObjectActionDelegate, IWorkbenchWindowActionDelegate {
+    private static Logger logger = new Logger(IgBaseAction.class);
+
+    private Shell shell;
+    Shell getShell() {
+    	return shell;
+    }
+
+    public void init(IWorkbenchWindow window) {
+        IWorkbenchPart targetPart = window.getActivePage().getActivePart();
+        shell = targetPart.getSite().getShell();
+    }
+
+    public void selectionChanged(IAction action, ISelection selection) {
+        // Do nothing by default.
+    }
+
+    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+        logger.fine("in setActivePart");
+        shell = targetPart.getSite().getShell();
+    }
+
+    public void dispose() {
+        // Do nothing by default.
+    }
+
+    IgCredentials retrieveCredentials(IgCredentialsDialog dialog) {
+        IgCredentials igCredentials = null;
+        if (IgCredentials.hasCurrentInstance()) {
+            igCredentials = IgCredentials.getCurrentInstance();
+        } else {
+            String username = dialog.getUsername();
+            String password = dialog.getPassword();
+            try {
+                igCredentials = IgCredentials.createCurrentInstance(username, password);
+            } catch (IgException e) {
+                logger.error("Invalid iGoogle Credentials (username, password)");
+            }
+        }
+        return igCredentials;
+    }
+}
---
+++ plugin_2010/0515b/src/com/googlecode/osde/internal/igoogle/IgCredentialsDialog.java
@@ -0,0 +1,180 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under
+ * the License.
+ */
+package com.googlecode.osde.internal.igoogle;
+
+import com.googlecode.osde.internal.utils.Logger;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * The dialog asking for users authentication info and hosting project name.
+ *
+ * @author albert.cheng.ig@gmail.com
+ */
+public class IgCredentialsDialog extends TitleAreaDialog {
+    private static Logger logger = new Logger(IgCredentialsDialog.class);
+
+    private boolean usePreviousIgCredentials;
+    private Button usePreviousIgCredentialsButton;
+    private String username;
+    private Text usernameText;
+    private String password;
+    private Text passwordText;
+    private String hostProjectName;
+    private Text hostProjectNameText;
+
+    public IgCredentialsDialog(Shell shell) {
+        super(shell);
+    }
+
+    void prepareComposite(Composite dialogAreaComposite) {
+    	logger.fine("in prepareComposite");
+
+        // Prepare credentialsComposite.
+        Composite credentialsComposite = new Composite(dialogAreaComposite, SWT.NONE);
+        credentialsComposite.setLayout(new GridLayout(3, true));
+        credentialsComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+        // Prepare host-project-name.
+        Label hostProjectNameLabel = new Label(credentialsComposite, SWT.LEFT);
+        hostProjectNameLabel.setText("Project name: ");
+        hostProjectNameLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
+        hostProjectNameText = new Text(credentialsComposite, SWT.SINGLE);
+        hostProjectNameText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+        hostProjectNameText.setText(IgPreviewJob.HOST_PROJECT_NAME_FOR_PREVIEW);
+        hostProjectNameText.setFocus();
+
+        // Prepare useCurrentIgCredentialsButton.
+        usePreviousIgCredentialsButton = new Button(credentialsComposite, SWT.RADIO);
+        usePreviousIgCredentialsButton.setText("Use the previous Google account session");
+        usePreviousIgCredentialsButton.setLayoutData(
+                new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
+
+        // Prepare useNewIgCredentialsButton.
+        Button useNewIgCredentialsButton = new Button(credentialsComposite, SWT.RADIO);
+        useNewIgCredentialsButton.setText("Use the following Google account");
+        useNewIgCredentialsButton.setLayoutData(
+                new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
+
+        // Prepare usernamePasswordComposite.
+        Composite usernamePasswordComposite = new Composite(dialogAreaComposite, SWT.NONE);
+        usernamePasswordComposite.setLayout(new GridLayout(3, true));
+        usernamePasswordComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+        // Prepare username.
+        Label usernameLabel = new Label(usernamePasswordComposite, SWT.LEFT);
+        usernameLabel.setText("\tUsername: ");
+        usernameLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
+        usernameText = new Text(usernamePasswordComposite, SWT.SINGLE);
+        usernameText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+
+        // Prepare password.
+        Label passwordLabel = new Label(usernamePasswordComposite, SWT.LEFT);
+        passwordLabel.setText("\tPassword: ");
+        passwordLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
+        passwordText = new Text(usernamePasswordComposite, SWT.SINGLE);
+        passwordText.setEchoChar('*');
+        passwordText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+
+        // Prepare description for authentication in HTTPS.
+        Label authenticationDescriptionLabel = new Label(usernamePasswordComposite, SWT.LEFT);
+        authenticationDescriptionLabel
+                .setText("(The authentication is processed via a secured HTTPS\nconnection.)");
+        authenticationDescriptionLabel
+                .setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
+
+        // Prepare selection listeners for radio buttons.
+        usePreviousIgCredentialsButton.addSelectionListener(new SelectionListener() {
+            public void widgetDefaultSelected(SelectionEvent e) {
+                // Do nothing.
+            }
+
+            public void widgetSelected(SelectionEvent e) {
+                usernameText.setEnabled(false);
+                passwordText.setEnabled(false);
+                hostProjectNameText.setFocus();
+            }
+        });
+        useNewIgCredentialsButton.addSelectionListener(new SelectionListener() {
+            public void widgetDefaultSelected(SelectionEvent e) {
+                // Do nothing.
+            }
+
+            public void widgetSelected(SelectionEvent e) {
+                usernameText.setEnabled(true);
+                passwordText.setEnabled(true);
+                usernameText.setFocus();
+            }
+        });
+
+        // Configure "selection" and "enabled" initially.
+        boolean hasCurrentIgCredentials = IgCredentials.hasCurrentInstance();
+        if (hasCurrentIgCredentials) {
+            usePreviousIgCredentialsButton.setEnabled(true);
+            usePreviousIgCredentialsButton.setSelection(true);
+            usernameText.setEnabled(false);
+            passwordText.setEnabled(false);
+        } else {
+            usePreviousIgCredentialsButton.setEnabled(false);
+            useNewIgCredentialsButton.setSelection(true);
+        }
+    }
+
+    @Override
+    protected Point getInitialSize() {
+        return new Point(350, 350);
+    }
+
+    @Override
+    protected void okPressed() {
+        usePreviousIgCredentials = usePreviousIgCredentialsButton.getSelection();
+        username = usernameText.getText();
+        password = passwordText.getText();
+        hostProjectName = hostProjectNameText.getText();
+        setReturnCode(OK);
+        close();
+    }
+
+    boolean isUsePreviousIgCredentials() {
+        return usePreviousIgCredentials;
+    }
+
+    String getUsername() {
+        return username;
+    }
+
+    String getPassword() {
+        return password;
+    }
+
+    String getHostProjectName() {
+        return hostProjectName;
+    }
+}
