03568c1ec94bbc4ee9b9c30d1ad36d0141d4168b
albert.cheng.ig <albert.cheng.ig@fa0fd100-0148-11de-ada4-5153b8187bf2>
2010-06-17 06:18:10 +0000

    tag v0.9.5

---
+++ tags/0.9.5/osde_plugin_test/src/com/googlecode/osde/internal/builders/GadgetBuilderTest.java
@@ -0,0 +1,36 @@
+package com.googlecode.osde.internal.builders;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.googlecode.osde.internal.builders.GadgetBuilder;
+
+/**
+ * TODO: add javadoc for type
+ */
+public class GadgetBuilderTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link GadgetBuilder#GadgetBuilder()}.
+     */
+    @Test
+    public void testGadgetBuilder() {
+        // TODO: implement test method
+    }
+
+    /**
+     * Test method for {@link GadgetBuilder#build(int, java.util.Map, org.eclipse.core.runtime.IProgressMonitor)}.
+     */
+    @Test
+    public void testBuildIntMapIProgressMonitor() {
+        // TODO: implement test method
+    }
+
+}
